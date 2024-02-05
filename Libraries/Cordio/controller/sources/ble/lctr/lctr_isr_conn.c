d0950fcdee527ec0b7c71a9e003662c73dc4169c
Ying Cai <ying.cai@analog.com>
2024-02-05 13:57:38 -0600

    llc_api_tx_start failed

--- Libraries/Cordio/controller/sources/ble/lctr/lctr_isr_conn.c
+++ Libraries/Cordio/controller/sources/ble/lctr/lctr_isr_conn.c
@@ -311,20 +311,22 @@ uint16_t lctrSetupForTx(lctrConnCtx_t *pCtx, uint8_t rxStatus, bool_t reqTx)
       lctrSetBbPacketCounterTx(pCtx);
       BbBleTxData(&bbDesc[0], bbDescCnt);
       numTxBytes = LL_DATA_HDR_LEN + bbDesc[0].pBuf[LCTR_DATA_PDU_LEN_OFFSET];
+      
       //PRINT_BLE_BUF(TX, bbDesc[0].pBuf, bbDesc[0].len);
-      
-      gu32DbgCharBufNdx += sprintf((char *)&gu8DbgCharBuf[gu32DbgCharBufNdx], "T: ");
-      for (uint32_t i = 0; i < bbDesc[0].len && gu32DbgCharBufNdx < DBG_CHAR_BUF_SIZE - 4; ++i)
+      if (gu32DbgCharBufNdx + 80 < DBG_CHAR_BUF_SIZE)
       {
-        gu32DbgCharBufNdx += sprintf((char *)&gu8DbgCharBuf[gu32DbgCharBufNdx], "%02X ", bbDesc[0].pBuf[i]);
+        gu32DbgCharBufNdx += sprintf((char *)&gu8DbgCharBuf[gu32DbgCharBufNdx], "%d T: ", PalBbGetCurrentTime());
+        for (uint32_t i = 0; i < bbDesc[0].len && gu32DbgCharBufNdx < DBG_CHAR_BUF_SIZE - 4; ++i)
+        {
+          gu32DbgCharBufNdx += sprintf((char *)&gu8DbgCharBuf[gu32DbgCharBufNdx], "%02X ", bbDesc[0].pBuf[i]);
+        }
+        if (gu32DbgCharBufNdx < DBG_CHAR_BUF_SIZE - 2)
+        {
+          gu8DbgCharBuf[gu32DbgCharBufNdx++] = '\r';
+          gu8DbgCharBuf[gu32DbgCharBufNdx++] = '\n';
+        }
+        gu8DbgCharBuf[DBG_CHAR_BUF_SIZE - 1] = 0;
       }
-      if (gu32DbgCharBufNdx < DBG_CHAR_BUF_SIZE - 2)
-      {
-        gu8DbgCharBuf[gu32DbgCharBufNdx++] = '\r';
-        gu8DbgCharBuf[gu32DbgCharBufNdx++] = '\n';
-      }
-      gu8DbgCharBuf[DBG_CHAR_BUF_SIZE - 1] = 0;
-      
     }
     else
     {
